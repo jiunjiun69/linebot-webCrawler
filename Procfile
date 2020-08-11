0114cdf587001b90ac3f174859538bb2b4c31bdc
jiunjiun <a902505@gmail,com>
2020-08-12 05:02:08 +0800

    My first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flask_line-bot:app –preload
---
+++ flask_line-bot.py
@@ -0,0 +1,71 @@
+# 載入需要的模組
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import time
+from linebot.exceptions import LineBotApiError
+
+import configparser
+
+# import json
+# from datetime import datetime
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# from flask import Flask, Response, render_template
+
+
+app = Flask(__name__)
+
+# cred = credentials.Certificate('serviceAccount.json')
+# firebase_admin.initialize_app(cred)
+
+
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token')) #聊天機器人的 Chennel access token
+handler = WebhookHandler(config.get('line-bot', 'channel_secret')) #聊天機器人的 Channel secret
+
+
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 學你說話，存user_id
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef": #忽略line的webhook驗證時的錯誤
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+        user_id = event.source.user_id #存user_id
+        push_message(user_id) #開始進行主動推播訊息
+        
+
+#主動推播
+def push_message(user_id):
+    # push message to one user
+    line_bot_api.push_message(user_id, TextSendMessage(text='Hello World!'))
+
+
+if __name__ == "__main__":
+    app.run()
