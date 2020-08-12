aabed79083befc7f354eb6e5adc7a38299dc6286
jiunjiun <a902505@gmail,com>
2020-08-12 09:40:28 +0800

    add dd

--- flask_line-bot.py
+++ flask_line-bot.py
@@ -142,18 +142,6 @@ def weather():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #用"USER ID or GROUP ID"去存要傳給誰的ID(個人還是群組)
-    if event.message.text == 'ID?' or event.message.text == 'id?':
-        User_ID = TextMessage(text=event.source.user_id)
-        line_bot_api.reply_message(event.reply_token, User_ID)
-        print ('Reply User ID =>' + event.source.user_id)
-    elif event.message.text == 'GroupID?':
-        Group_ID = TextMessage(text=event.source.group_id)
-        line_bot_api.reply_message(event.reply_token, Group_ID)
-        print ('Reply Group ID =>' + event.source.group_id)
-    else:
-        None
-
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
 
@@ -233,7 +221,11 @@ def handle_message(event):
                 for i in img:
                     #傳出圖檔
                     url = 'https:' + i + '.jpg'
-                    line_bot_api.push_message("USER ID or GROUP ID", ImageSendMessage(original_content_url = url, preview_image_url = url))
+                    message = ImageSendMessage(
+                        original_content_url=url,
+                        preview_image_url=url
+                    )
+                    line_bot_api.reply_message(event.reply_token, message)
                     #傳出圖檔
         
             else: 
