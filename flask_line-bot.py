85fbf05e88957db39a190b6d053849e2c713de5c
jiunjiun <a902505@gmail,com>
2020-08-12 09:08:32 +0800

    add dd

--- flask_line-bot.py
+++ flask_line-bot.py
@@ -222,7 +222,7 @@ def handle_message(event):
                 content = '搜索到了!'
                 for i in img:
                     #傳出圖檔
-                    url = 'https:' + i
+                    url = 'https:' + i + '.jpg'
                     line_bot_api.reply_message(event.source.user_id, ImageSendMessage(original_content_url = url, preview_image_url = url))
                     #傳出圖檔
         
